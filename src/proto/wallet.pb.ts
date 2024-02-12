7d4bbeff289425f3519aa65e5e4c19511a6f3ce8
Isoboye Ibanibo <elsopi02@yahoo.com>
2024-02-12 07:39:19 +0100

    updates

--- src/proto/wallet.pb.ts
+++ src/proto/wallet.pb.ts
@@ -51,7 +51,7 @@ export interface GetPaymentMethodResponse {
   success: boolean;
   status: number;
   message: string;
-  data: PaymentMethod[];
+  data?: PaymentMethod[];
 }
 
 export interface PaymentMethodResponse {
