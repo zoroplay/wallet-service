42dd1221722d573f3d1314cd03069a26fd64d313
Isoboye Ibanibo <elsopi02@yahoo.com>
2024-09-19 06:10:11 +0100

    debugging withdrawal with Paycom

--- src/services/withdrawal.service.ts
+++ src/services/withdrawal.service.ts
@@ -95,7 +95,7 @@ export class WithdrawalService {
       jobData.withdrawalCode = generateTrxNo();
       jobData.balance = wallet.available_balance;
 
-      console.log('adding to withdrawal queue', jobData)
+      // console.log('adding to withdrawal queue', jobData)
       await this.withdrawalQueue.add('withdrawal-request', jobData, {
         jobId: `${data.userId}:${data.clientId}:${data.accountNumber}:${data.amount}`,
       });
