d529f787211277057eb9a07469eddd6444540303
Isoboye Ibanibo <elsopi02@yahoo.com>
2024-11-09 02:17:42 +0100

    fix for wayaquick

--- src/services/wayaquick.service.ts
+++ src/services/wayaquick.service.ts
@@ -69,7 +69,7 @@ export class WayaQuickService {
       );
       console.log('wayaquick response', res)
       // if transaction is valid with wayaquick, proceed
-      if (res.status && res.data.Status === 'APPROVED') {
+      if (res.status && res.data.Status === 'SUCCESSFUL') {
         // fetch transaction internally
         const transaction = await this.transactionRepository.findOne({
           where: {
